29aa031de807a0a6c0d6807c5389e20449f0eac1
CarinaoHC <karinaina678@gmail.com>
2024-03-11 13:48:44 -0400

    clase 11/3/24

--- semana3/graphql/gql_client.py
+++ semana3/graphql/gql_client.py
@@ -1,38 +1,115 @@
 import requests
+# Definir la URL del servidor GraphQL
+url = 'http://localhost:8000/graphql'
 
-# Definir la consulta GraphQL
-query = """
-    {
+# Definir la consulta GraphQL simple
+query_lista = """
+{
         estudiantes{
             id
             nombre
             apellido
             carrera
         }
-        
     }
 """
-query2 = """
-    {
-        estudiantes{
-            nombre
-        }
-        
-    }
-"""
-query3 = """
-    {
-        estudiantes{
-            nombre
-            apellido
-        }
-        
-    }
-"""
-
-# Definir la URL del servidor GraphQL
-url = "http://localhost:8000/graphql"
-
 # Solicitud POST al servidor GraphQL
-response = requests.post(url, json={"query": query})
+response = requests.post(url, json={'query': query_lista})
+print(response.text)
+
+# Definir la consulta GraphQL para crear nuevo estudiante 1
+query_crear = """
+mutation {
+        crearEstudiante(nombre: "Angel", apellido: "Gomez", carrera: "Biologia") {
+            estudiante {
+                id
+                nombre
+                apellido
+                carrera
+            }
+        }
+    }
+"""
+
+response_mutation = requests.post(url, json={'query': query_crear})
+print(response_mutation.text)
+
+# Definir la consulta GraphQL para crear nuevo estudiante 2
+query_crear2 = """
+mutation {
+        crearEstudiante(nombre: "Maria", apellido: "Perez", carrera: "Arquitectura") {
+            estudiante {
+                id
+                nombre
+                apellido
+                carrera
+            }
+        }
+    }
+"""
+
+response_mutation2 = requests.post(url, json={'query': query_crear2})
+print(response_mutation2.text)
+
+# Definir la consulta GraphQL para crear nuevo estudiante 1
+query_crear3 = """
+mutation {
+        crearEstudiante(nombre: "Oscar", apellido: "Iglesias", carrera: "Matematicas") {
+            estudiante {
+                id
+                nombre
+                apellido
+                carrera
+            }
+        }
+    }
+"""
+
+response_mutation3 = requests.post(url, json={'query': query_crear3})
+print(response_mutation3.text)
+
+# Lista de todos los estudiantes
+response = requests.post(url, json={'query': query_lista})
+print(response.text)
+
+# Definir la consulta GraphQL para eliminar un estudiante
+query_eliminar = """
+mutation {
+        deleteEstudiante(id: 3) {
+            estudiante {
+                id
+                nombre
+                apellido
+                carrera
+            }
+        }
+    }
+"""
+
+response_mutation = requests.post(url, json={'query': query_eliminar})
+print(response_mutation.text)
+
+# Lista de todos los estudiantes
+response = requests.post(url, json={'query': query_lista})
+print(response.text)
+
+
+query_actualizar = """
+mutation {
+        actualizarEstudiante(nombre: "Jose", apellido: "Lopez) {
+            estudiante {
+                id
+                nombre
+                apellido
+                carrera
+            }
+        }
+    }
+"""
+
+response_mutation_ac = requests.post(url, json={'query': query_actualizar})
+print(response_mutation_ac.text)
+
+# Lista de todos los estudiantes
+response = requests.post(url, json={'query': query_lista})
 print(response.text)
--- semana3/graphql/gql_server.py
+++ semana3/graphql/gql_server.py
@@ -1,6 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
-from graphene import ObjectType, String, Int, List, Schema, Field
+from graphene import ObjectType, String, Int, List, Schema, Field, Mutation
 
 
 class Estudiante(ObjectType):
@@ -9,23 +9,81 @@ class Estudiante(ObjectType):
     apellido = String()
     carrera = String()
 
-
-class Query(ObjectType):
-    estudiantes = List(Estudiante)
-
-    def resolve_estudiantes(root, info):
-        return estudiantes
-
 estudiantes = [
     Estudiante(
         id=1, nombre="Pedrito", apellido="García", carrera="Ingeniería de Sistemas"
     ),
-    Estudiante(
-        id=2, nombre="Jose", apellido="Lopez", carrera="Arquitectura"
-    ),
+    Estudiante(id=2, nombre="Jose", apellido="Lopez", carrera="Arquitectura"),
 ]
 
-schema = Schema(query=Query)
+class Query(ObjectType):
+    estudiantes = List(Estudiante)
+    estudiante_por_id = Field(Estudiante, id=Int())
+    
+    def resolve_estudiantes(root, info):
+        return estudiantes
+
+    def resolve_estudiante_por_id(root, info, id):
+        for estudiante in estudiantes:
+            if estudiante.id == id:
+                return estudiante
+        return None
+
+class CrearEstudiante(Mutation):
+    class Arguments:
+        nombre = String()
+        apellido = String()
+        carrera = String()
+
+    estudiante = Field(Estudiante)
+
+    def mutate(root, info, nombre, apellido, carrera):
+        nuevo_estudiante = Estudiante(
+            id=len(estudiantes) + 1, 
+            nombre=nombre, 
+            apellido=apellido, 
+            carrera=carrera
+        )
+        estudiantes.append(nuevo_estudiante)
+
+        return CrearEstudiante(estudiante=nuevo_estudiante)
+
+class DeleteEstudiante(Mutation):
+    class Arguments:
+        id = Int()
+
+    estudiante = Field(Estudiante)
+
+    def mutate(root, info, id):
+        for i, estudiante in enumerate(estudiantes):
+            if estudiante.id == id:
+                estudiantes.pop(i)
+                return DeleteEstudiante(estudiante=estudiante)
+        return None
+    
+class ActualizarEstudiante(Mutation):
+    class Arguments:
+        nombre = String()
+        apellido = String()
+
+    estudiante = Field(Estudiante)
+
+    def mutate(root, info, nombre, apellido):
+        for i, estudiante in enumerate(estudiantes):
+            if estudiante.nombre == nombre and estudiante.apellido == apellido:
+                estudiante.carrera = "Antropología"
+                return ActualizarEstudiante(estudiante=estudiante)
+        return None
+    
+
+class Mutations(ObjectType):
+    crear_estudiante = CrearEstudiante.Field()
+    delete_estudiante = DeleteEstudiante.Field()
+    actualizar_estudiante = ActualizarEstudiante.Field()
+
+
+
+schema = Schema(query=Query, mutation=Mutations)
 
 
 class GraphQLRequestHandler(BaseHTTPRequestHandler):
@@ -40,6 +98,7 @@ class GraphQLRequestHandler(BaseHTTPRequestHandler):
             content_length = int(self.headers["Content-Length"])
             data = self.rfile.read(content_length)
             data = json.loads(data.decode("utf-8"))
+            print(data)
             result = schema.execute(data["query"])
             self.response_handler(200, result.data)
         else:
