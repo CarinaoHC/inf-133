40ba07c362a398b98ed2312b3451427d2015333c
CarinaoHC <karinaina678@gmail.com>
2024-05-06 17:37:15 -0400

    Avance 06/05/2024

---
+++ semana9/mvc_api_lib/app/run.py
@@ -0,0 +1,34 @@
+
+from flask import Flask
+from controllers.book_controller import book_bp
+from flask_swagger_ui import get_swaggerui_blueprint
+from database import db
+
+app = Flask(__name__)
+
+# Configura la URL de la documentación OpenAPI
+SWAGGER_URL = "/api/docs"  # Ruta para servir Swagger UI
+API_URL = "/static/openapi.yaml"  # Ruta de tu archivo OpenAPI/Swagger
+# Inicializa el Blueprint de Swagger UI
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL, API_URL, config={"app_name": "Libros API"}
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
+# Configuración de la base de datos
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///lib.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Inicializa la base de datos
+db.init_app(app)
+
+# Registra el blueprint de books en la aplicación
+app.register_blueprint(book_bp, url_prefix="/api")
+
+# Crea las tablas si no existen
+with app.app_context():
+    db.create_all()
+
+# Ejecuta la aplicación
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ semana9/mvc_api_lib/client.py
@@ -0,0 +1,60 @@
+import requests
+
+# URL base de la API
+BASE_URL = "http://localhost:5000/api"
+
+# Definir los encabezados de la solicitud
+headers = {"Content-Type": "application/json"}
+
+# Crear un nuevo book
+url = f"{BASE_URL}/books"
+nuevo_book = {"title": "Oceano", "author": "Oscar", "edition": 3, "availability": True}
+response = requests.post(url, json=nuevo_book, headers=headers)
+print("Creando un nuevo book:")
+print(response.json())
+
+# Crear el segundo book
+book_2 = {"title": "La paciencia", "author": "Ana", "edition": 4, "availability": False}
+response = requests.post(url, json=book_2, headers=headers)
+print("\nCreando el segundo book:")
+print(response.json())
+
+# Obtener la lista de todos los bookes
+url = f"{BASE_URL}/books"
+response = requests.get(url, headers=headers)
+print("\nLista de books:")
+print(response.json())
+
+# Obtener un book específico por su ID (por ejemplo, ID=1)
+url = f"{BASE_URL}/books/1"
+response = requests.get(url, headers=headers)
+print("\nDetalles del book con ID 1:")
+print(response.json())
+
+# Actualizar un book existente por su ID (por ejemplo, ID=1)
+url = f"{BASE_URL}/books/1"
+datos_actualizacion = {"title": "Lagrimas", "author": "Lidia", "edition": 3, "availability": True}
+response = requests.put(url, json=datos_actualizacion, headers=headers)
+print("\nActualizando el book con ID 1:")
+print(response.json())
+
+# Obtener la lista de todos los bookes
+url = f"{BASE_URL}/books"
+response = requests.get(url, headers=headers)
+print("\nLista de books:")
+print(response.json())
+
+# Eliminar un book existente por su ID (por ejemplo, ID=1)
+url = f"{BASE_URL}/books/1"
+response = requests.delete(url, headers=headers)
+print("\nEliminando el book con ID 1:")
+if response.status_code == 204:
+    print(f"book con ID 1 eliminado con éxito.")
+else:
+    print(f"Error: {response.status_code} - {response.text}")
+    
+# Obtener la lista de todos los bookes
+url = f"{BASE_URL}/books"
+response = requests.get(url, headers=headers)
+print("\nLista de books:")
+print(response.json())
